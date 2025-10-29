ab08585011b5422c70083eb0ac1e020e333ddff2
BILL-CHEPTOYEK <billcheptoyek60@gmail.com>
2025-10-29 08:53:11 +0300

    incremeant

--- src/components/Greeting.jsx
+++ src/components/Greeting.jsx
@@ -52,7 +52,7 @@ const Greeting = () => {
             onClick={handleIncrement}
             className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600 transition active:scale-95 focus:outline-none focus:ring-2 focus:ring-green-300"
           >
-            Increment
+            Incremen
           </button>
 
           <button
